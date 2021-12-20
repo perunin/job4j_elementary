f3660208bc80793e2f01fbbbfa2e47e2218d0aa2
perunin <perunin@icloud.com>
2021-12-20 12:23:37 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -9,13 +9,13 @@ public class Converter {
     public static float rubleToDollar(float value) {
         float rsl = value / 60;
         return rsl;
-}
+    }
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         float dollar = Converter.rubleToDollar(180);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("180 rubles are " + dollar + " $.");
 
-}
+    }
 }
