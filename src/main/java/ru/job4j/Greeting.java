60bddbbb54095f6361b2ef3def4065077e6e67e6
perunin <perunin@icloud.com>
2021-12-21 14:38:04 +0300

    9.1. String [#237407]

--- src/main/java/ru/job4j/Greeting.java
+++ src/main/java/ru/job4j/Greeting.java
@@ -3,9 +3,10 @@ package ru.job4j;
 public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java ";
-        System.out.println(idea + "But I am a newbie");
+        System.out.println(idea);
+        idea += "But I am a newbie";
         int year = 2021;
-        idea = idea + year;
+        idea += year;
         System.out.println(idea);
 
     }
