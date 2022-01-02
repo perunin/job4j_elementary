9dfc6ba510bb37db24ba8363545b152e070cb66e
perunin <perunin@icloud.com>
2022-01-02 18:20:19 +0300

    Входные данные в тесте [#297750 #246148]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -54,8 +54,8 @@ public class X2Test {
         int a = 1;
         int b = 1;
         int c = 1;
-        int x = 0;
-        int expected = 1;
+        int x = 3;
+        int expected = 13;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
