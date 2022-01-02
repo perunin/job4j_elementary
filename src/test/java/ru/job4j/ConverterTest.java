2acf5a6d98c995d539811bc4193f6c64d8293784
perunin <perunin@icloud.com>
2022-01-02 11:51:20 +0300

    Модульные тесты. [#159435 #246005]

---
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConverter140rblThen2euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConverter180RbThen3Dlr() {
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        double eps = 0.0001;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
