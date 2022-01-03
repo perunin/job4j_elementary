4502f888086e7905f3c0033287f178321766db25
perunin <perunin@icloud.com>
2022-01-03 22:42:57 +0300

    Math.abs

---
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+
+    public static  int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
+        int x = Math.abs(x1 - x2);
+        int y = Math.abs(y1 - y2);
+        boolean rightstep = (x == y);
+        if (isValid(x1) && isValid(x2)
+                && isValid(y1) && isValid(y2)) {
+            if (rightstep) {
+                rsl = Math.abs(x2 - x1);
+                rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean isValid(int coordinate) {
+        return coordinate >= 0 && coordinate <= 7;
+    }
+}
