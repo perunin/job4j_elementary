50fcfbc66a93509171d7cbb5ae606fc5fbbbed7c
perunin <perunin@icloud.com>
2022-01-04 10:25:19 +0300

    4.2 Тернарное сравнение

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean condition = left > right;
-        int result = condition ? left : right;
+        int result = left > right ? left : right;
         return result;
     }
 
