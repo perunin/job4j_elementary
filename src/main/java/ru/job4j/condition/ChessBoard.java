d53a0df3b79557258bf6ad46e07c84d416d4e50f
perunin <perunin@icloud.com>
2022-01-04 10:31:30 +0300

    4.2 Тернарное сравнение

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -4,15 +4,13 @@ public class ChessBoard {
 
     public static  int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        int x = Math.abs(x1 - x2);
-        int y = Math.abs(y1 - y2);
-        boolean rightstep = (x == y);
+        //int x = Math.abs(x1 - x2);
+        //int y = Math.abs(y1 - y2);
+        //boolean rightstep = (x == y);
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (rightstep) {
-                rsl = Math.abs(x);
-            } else {
-                return rsl;
+            if (Math.abs(x1 - x2) == (Math.abs(y1 - y2))) {
+                rsl = Math.abs(x1 - x2);
             }
         }
         return rsl;
