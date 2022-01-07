fb3aae164ccf7bb8f7152eea12c1d74b4ccaf9e1
perunin <perunin@icloud.com>
2022-01-07 14:53:02 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192 #247852] v2

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -23,9 +23,9 @@ public class Counter {
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
 
-        System.out.println(sumByEven(0,10));
+        System.out.println(sumByEven(0, 10));
         System.out.println(sumByEven(3, 8));
-        System.out.println(sumByEven(1,1));
+        System.out.println(sumByEven(1, 1));
     }
 }
 
