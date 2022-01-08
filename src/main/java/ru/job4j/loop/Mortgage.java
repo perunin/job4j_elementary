943380e5e638f53cba363b5f23068c4b4af6ae5f
perunin <perunin@icloud.com>
2022-01-08 14:05:03 +0300

    5.7. Ипотека [#156315 #248098] v3

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         percent /= 100;
-        while (0 < (amount + (amount * percent))) {
+        while (amount > 0) {
             amount = (amount + (amount * percent)) - salary;
             year++;
         }
