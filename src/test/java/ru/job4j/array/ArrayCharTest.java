9553ab9c72e0d9be55e0422eff2e68b41d5b4091
perunin <perunin@icloud.com>
2022-01-10 21:10:05 +0300

    6.4. Слово начинается с ... [#41585

---
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayCharTest {
+
+    @Test
+    public void startWith() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'e'};
+        boolean result = ArrayChar.startWith(word, pref);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'i'};
+        boolean result = ArrayChar.startWith(word, pref);
+        Assert.assertFalse(result);
+    }
+}
