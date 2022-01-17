3e30630ff1aeba2e9962ec1621fd1ee7442e3657
perunin <perunin@icloud.com>
2022-01-17 11:39:12 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине v5

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         money -= price;
-        for (int i = 0; i < coins.length; i++) {
-            while (money >= coins[i]) {
-                rsl[size] = coins[i];
-                money -= coins[i];
+        for (int coin : coins) {
+            while (money >= coin) {
+                rsl[size] = coin;
+                money -= coin;
                 size++;
             }
 
